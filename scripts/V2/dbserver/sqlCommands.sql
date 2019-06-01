35927379996842669f661513ef567edf923f54c4
Ninad Kanthi <ninad_kanthi@hotmail.com>
2019-06-01 14:35:57 +0100

    Removed spurious  bat files

--- scripts/V2/dbserver/sqlCommands.sql
+++ scripts/V2/dbserver/sqlCommands.sql
@@ -1,14 +1,14 @@
 CREATE DATABASE wordpress;
 FLUSH PRIVILEGES;
-CREATE USER 'wpuser'@'localhost' IDENTIFIED BY '<<InsertPasswordHere>>';
+CREATE USER 'wpuser'@'localhost' IDENTIFIED BY 'Dietpepsi-99!';
 FLUSH PRIVILEGES;
 GRANT ALL PRIVILEGES ON wordpress.* TO 'wpuser'@'localhost';
 FLUSH PRIVILEGES;
-CREATE USER 'remotewpuser'@'10.0.1.4' IDENTIFIED BY '<<InsertPasswordHere>>';
+CREATE USER 'remotewpuser'@'10.0.1.4' IDENTIFIED BY 'Dietpepsi-99!';
 FLUSH PRIVILEGES;
 GRANT ALL PRIVILEGES ON wordpress.* TO 'remotewpuser'@'10.0.1.4';
 FLUSH PRIVILEGES;
-CREATE USER 'remotewpuser'@'10.0.1.5' IDENTIFIED BY '<<InsertPasswordHere>>';
+CREATE USER 'remotewpuser'@'10.0.1.5' IDENTIFIED BY 'Dietpepsi-99!';
 FLUSH PRIVILEGES;
 GRANT ALL PRIVILEGES ON wordpress.* TO 'remotewpuser'@'10.0.1.5';
 FLUSH PRIVILEGES;
--- scripts/V2/webuser/install-ssl.sh
+++ scripts/V2/webuser/install-ssl.sh
@@ -3,24 +3,38 @@
 echo 'Important: Before executing this script you set the https from wordpress admin site'
 
 echo 'installing and configuring the certificates on the machine............'
-# ECHO if installing from letscertify, uncomment the following sections
+# if installing from letscertify, uncomment the following section
 # ================  Lets encrypt SSL                  ========================
+# echo '            rtificate installation from letscertify ..................'
 #sudo apt-get install -y software-properties-common
 #sudo add-apt-repository ppa:certbot/certbot 
 #sudo apt-get update 
 #sudo apt-get install -y certbot
+# uncomment one of the followinc commands
 #sudo certbot certonly --dry-run --webroot -w /var/www/html/wordpress -d blogs.ninadkanthi.co.uk
 #sudo certbot certonly --webroot -w /var/www/html/wordpress -d blogs.ninadkanthi.co.uk
 #sudo chown -R webuser:webuser /etc/letsencrypt/live
 #sudo chown -R www-data:www-data /etc/letsencrypt/
+#cat /etc/letsencrypt/live/blogs.ninadkanthi.co.uk/README
+# echo '............certificate installation fine from letscertify '
 # ================  Lets encrypt SSL                  ========================
-
 
 sudo chown -R webuser:webuser /var/log/apache2
 sudo chown -R www-data:www-data /var/www/html
 
+
+
+
+echo '...............  creating back-up copy og default-ssl.conf    -----------'
+sudo cp /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-available/wordpress-ssl.conf
+
+echo '...............  substituting values in wordpress-ssl.conf    -----------'
+sudo python py_file_replace_str.py "/etc/apache2/sites-available/wordpress-ssl.conf" "ServerAdmin webmaster@localhost"  "ServerAdmin webmaster@localhost \n\t\t\tServerName ninadkanthi.co.uk \n\t\t\tServerAlias www.ninadkanthi.co.uk"
+sudo python py_file_replace_str.py "/etc/apache2/sites-available/wordpress-ssl.conf" "DocumentRoot /var/www/html" "DocumentRoot /var/www/html/wordpress"
+
 # If installing from pre-configured SSL files uncomment following lines
 # ================  Pre-configured SSL                  ========================
+echo '       configuring SSL for pre-created SSL files    -----------'
 sudo apt-get update 
 sudo mkdir /etc/ninadkanthi.co.uk
 sudo chown -R webuser:webuser /etc/ninadkanthi.co.uk
@@ -28,21 +42,15 @@ sudo chown -R www-data:www-data /etc/ninadkanthi.co.uk
 sudo cp *.crt /etc/ninadkanthi.co.uk
 sudo cp *.key /etc/ninadkanthi.co.uk 
 sudo cp *.csr /etc/ninadkanthi.co.uk
-# ================  Pre-configured SSL                  ========================
 
-
-#cat /etc/letsencrypt/live/blogs.ninadkanthi.co.uk/README:q!
-
-echo '.........................................certificate installation fine'
-echo '...............  creating back-up copy og default-ssl.conf    -----------'
-sudo cp /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-available/wordpress-ssl.conf
-
-echo '...............  substituting values in wordpress-ssl.conf    -----------'
-sudo python py_file_replace_str.py "/etc/apache2/sites-available/wordpress-ssl.conf" "ServerAdmin webmaster@localhost"  "ServerAdmin webmaster@localhost \n\t\t\tServerName ninadkanthi.co.uk \n\t\t\tServerAlias www.ninadkanthi.co.uk"
-sudo python py_file_replace_str.py "/etc/apache2/sites-available/wordpress-ssl.conf" "DocumentRoot /var/www/html" "DocumentRoot /var/www/html/wordpress"
 sudo python py_file_replace_str.py "/etc/apache2/sites-available/wordpress-ssl.conf" "/etc/ssl/certs/ssl-cert-snakeoil.pem" "/etc/ninadkanthi.co.uk/19d316058c23516e.crt"
 sudo python py_file_replace_str.py "/etc/apache2/sites-available/wordpress-ssl.conf" "/etc/ssl/private/ssl-cert-snakeoil.key" "/etc/ninadkanthi.co.uk/ninadkanthi.co.uk.key"
 sudo python py_file_replace_str.py "/etc/apache2/sites-available/wordpress-ssl.conf" "#SSLCertificateChainFile /etc/apache2/ssl.crt/server-ca.crt" "SSLCertificateChainFile /etc/ninadkanthi.co.uk/gd_bundle-g2-g1.crt"
+
+echo ' -------------- [DONE] configuring SSL for pre-created SSL files'
+
+# ================  Pre-configured SSL                  ========================
+
 echo '...............  substituting values in wordpress.conf    -----------'
 #sudo python py_file_replace_str.py "/etc/apache2/sites-available/wordpress.conf" "/etc/ssl/private/ssl-cert-snakeoil.key" "/etc/ninadkanthi.co.uk/ninadkanthi.co.uk.key"
 sudo python py_file_replace_str.py "/etc/apache2/sites-available/wordpress.conf" "#Include conf-available/serve-cgi-bin.conf" "#Include conf-available/serve-cgi-bin.conf \n\t\t\tRedirect / https://blogs.ninadkanthi.co.uk/"
